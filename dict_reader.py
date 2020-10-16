5266d6cd43a21a0fffb5fb41cbb8f1d139ef0da7
Arina Okshus <me@chameleon-lizard.ru>
2020-10-17 00:52:00 +0300

    Fixes in dict_reader.py, created buggy text_gen.py

--- dict_reader.py
+++ dict_reader.py
@@ -12,47 +12,53 @@ class Dict_reader:
     def get_scientific(self, amount = 1):
         """
         Gets a scientific term from the dictionary. 
-        Params: amount - amount of words that is being returned. By default is 1; if it is not 1, function returns a list of words.+
+
+        Params: amount - amount of words that is being returned. By default is 1.
+
         Return value: list of scientific terms.
         """
         if self.__scientific_read == 0:
             with open("ru/scientific.txt", 'r') as f:
                 self.__scientific = f.read()
+                self.__scientific = self.__scientific.split("\n")
 
-        self.__scientific = self.__scientific.split("\n")
-        result = self.__scientific[self.__scientific_read:amount]
+        result = self.__scientific[self.__scientific_read:self.__scientific_read + amount]
         self.__scientific_read += amount
-        return result
+        return list(result)
     
     def get_word(self, amount = 1):
         """
         Gets a random word from the dictionary. 
-        Params: amount - amount of words that is being returned. By default is 1; if it is not 1, function returns a list of words.
+
+        Params: amount - amount of words that is being returned. By default is 1.
+
         Return value: list of random words.
         """
         if self.__words_read == 0:
             with open("ru/words.txt", 'r') as f:
                 self.__words = f.read()
+                self.__words = self.__words.split("\n")
 
-        self.__words = self.__words.split("\n")
-        result = self.__words[self.__words_read:amount]
+        result = self.__words[self.__words_read:self.__words_read + amount]
         self.__words_read += amount
-        return result
+        return list(result)
 
     def get_name(self, amount = 1):
         """
         Gets a name from the dictionary. 
-        Params: amount - amount of words that is being returned. By default is 1; if it is not 1, function returns a list of words.
+
+        Params: amount - amount of words that is being returned. By default is 1.
+
         Return value: list of names.
         """
         if self.__names_read == 0:
             with open("ru/names.txt", 'r') as f:
                 self.__names = f.read()
+                self.__names = self.__names.split("\n")
 
-        self.__names = self.__names.split("\n")
-        result = self.__names[self.__names_read:amount]
+        result = self.__names[self.__names_read:self.__names_read + amount]
         self.__names_read += amount
-        return result
+        return list(result)
 
     def __init__(self):
         self.__MorphAnalyzer = pymorphy2.MorphAnalyzer(lang="ru")
--- text_gen.py
+++ text_gen.py
@@ -1,8 +1,61 @@
 # -*- coding: utf-8 -*-
+from nltk.corpus import stopwords
 import dict_reader
+import pymorphy2
+import nltk
 
+# Initializing and creating objects for later use
+morph = pymorphy2.MorphAnalyzer(lang="ru")
 dr = dict_reader.Dict_reader()
+stopwords_nltk = stopwords.words("russian")
+stop = set()
 
-print(dr.get_scientific(10))
-print(dr.get_name(20))
-print(dr.get_word(30))
+# Making our own set of stop words, because NLTK's list contains words not in normal from
+for word in stopwords_nltk:
+    stop.add(morph.parse(word)[0].normal_form)
+
+# Creating dict to store words
+translate_dict = dict()
+text = "Прошлое – это поезд, который ушёл. Будущее – это мечта, которая может сбыться. А настоящее – это просто мгновение, в котором мы живём."
+tokens = nltk.word_tokenize(text)
+
+# For statement in which we iterate over the text
+for token in tokens:
+    # Parsing the word
+    current_word = morph.parse(token)[0]
+    # Getting the original word's grammemes 
+    params = set(current_word.tag.grammemes)
+
+    # Checking if it's actually a word and if it's a stop-word
+    if "LATN" in params or "PNCT" in params or "NUMB" in params or "intg" in params or "real" in params or "ROMN" in params or "UNKN" in params or current_word.normal_form in stop:
+        continue
+
+    # Checking if it already exists in our dictionary
+    if current_word.normal_form in translate_dict:
+        tokens[tokens.index(token)] = dict_word.inflect(params)
+        continue
+
+    # Starting to cycle through the dictionary in search for compatible words
+    dict_word = ""
+    while True:
+        # If the word is name
+        if "Name" in params:
+            dict_word = morph.parse(dr.get_name()[0])[0]
+            
+            # Checking for unchangable parameters of the word
+            if dict_word.tag.POS in params and dict_word.tag.gender in params:
+                break
+        else:
+            kek = dr.get_word()
+            lol = morph.parse(kek[0])
+            dict_word = lol[0]
+            
+            # Checking for unchangable parameters of the word
+            if dict_word.tag.POS in params and dict_word.tag.gender in params and dict_word.tag.animacy in params:
+                break
+    
+    # Adding resulting word to the dictionary and replacing the original word
+    translate_dict[current_word.normal_form] = dict_word.normal_form
+    tokens[tokens.index(token)] = dict_word.inflect(params)
+
+print(tokens)
